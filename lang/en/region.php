c1d376d1d4285abbd90b3adb19d3a6ff5483cae5
Ernest E <ee@cnclub.asia>
2018-07-18 19:13:54 +0800

    🎉 Initial commit

---
+++ lang/en/region.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'n-virginia'       => 'N. Virginia',
+    'ohio'             => 'Ohio',
+    'n-california'     => 'N. California',
+    'oregon'           => 'Oregon',
+    'us-govcloud-west' => 'US GovCloud West',
+    'canada'           => 'Canada',
+    'ireland'          => 'Ireland',
+    'london'           => 'London',
+    'paris'            => 'Paris',
+    'frankfurt'        => 'Frankfurt',
+    'tokyo'            => 'Tokyo',
+    'seoul'            => 'Seoul',
+    'singapore'        => 'Singapore',
+    'sydney'           => 'Sydney',
+    'mumbai'           => 'Mumbai',
+    'sao-paulo'        => 'Sao Paulo',
+    'bejing'           => 'Beijing',
+    'ningxia'          => 'Ningxia',
+];
---
+++ lang/zh-CN/region.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'n-virginia' => '弗吉尼亚北部',
+    'ohio'       => '俄亥俄',
+
+];
